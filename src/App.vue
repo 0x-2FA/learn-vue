cc365f9c04fdb313f218c5c4c44cf0177b11b0e8
0x-2FA <2fa_gh@protonmail.com>
2021-12-09 12:34:49 +0200

    Cleared the default page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,4 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
-</template>
 
-<script>
-export default {
-  name: 'App',
-};
-</script>
 
 <style>
 #app {
